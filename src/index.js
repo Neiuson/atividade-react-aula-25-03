72d9c2b08433dd43eaa4e59c026a823f72fb6469
Neiuson <neiuson@gmail.com>
2022-03-25 21:48:21 -0300

    adicionado jsx

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     return (
-        <div>
-            Meu primeiro componente React.
-        </div>
+        <div style={{margin:'auto',with:768, backgroundColor :`#EEE`,padding: 12, borderRadius:8}}>
+        <label htmlFor="nome" style={{display: 'block',marginBottom: 4}}>Nome</label>
+        <input id= 'nome'type="text" style={{paddingTop: 12,paddingBottom: 8, borderStyle:'hidden',outline:'none',width:'100%',boxSizing}}></input>
+        <button style={{marginTop : 12,paddintop: 8,paddingBottom: 8, backgroundColor:'blueviolet' }}></button>
+        </div> 
     )
-}
-
+    
+    }
 
 ReactDOM.render(
     <App/>,
